4a2906e395c9eb02c04b375eb4eb253882f0eb74
aris <khaerulwaris94@gmail.com>
2023-03-17 10:47:33 +0800

    melakukan penghapusan style css dan menambah style di php

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>percobaan git</title>
-    <link rel="stylesheet" href="style.css">
+    <style type="text/css">
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
 </head>
 <body>
     <h1>ini percobaan menggunakan git secara lokal dalam komputer</h1>
