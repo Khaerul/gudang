0e8da03089f750f4a60400efd4dcb5ec6a6445a5
aris <khaerulwaris94@gmail.com>
2023-03-17 10:38:54 +0800

    memasukan code html:5

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>percobaan git</title>
+</head>
+<body>
+    <h1>ini percobaan menggunakan git secara lokal dalam komputer</h1>
+</body>
+</html>
